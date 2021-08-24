45f6a2eac4ed0ab1f092bbcc8cef9eee34f9caca
Grace Sutherland <78558344+rgs1690@users.noreply.github.com>
2021-08-23 19:14:51 -0500

    Initial commit

---
+++ src/javascripts/views/startApp.js
@@ -0,0 +1,19 @@
+import { showBooks } from '../components/books';
+import logoutButton from '../components/buttons/logoutButton';
+import domBuilder from '../components/domBuilder';
+import navBar from '../components/navBar';
+import domEvents from '../events/domEvents';
+import navigationEvents from '../events/navigationEvents';
+import getBooks from '../helpers/data/bookData';
+
+const startApp = () => {
+  domBuilder(); // BUILD THE DOM
+  domEvents(); // ADD THE EVENT LISTENTERS TO THE DOM
+  navBar(); // DYNAMICALLY ADD THE NAV
+  logoutButton(); // ADD THE LOGOUT BUTTON COMPONENT
+  navigationEvents(); // ATTACH THE EVENT LISTENERS TO THE NAVBAR
+  // Put all books on the DOM
+  getBooks().then((books) => showBooks(books));
+};
+
+export default startApp;
