8172e549510327b2ee5b43a6f2798f46587a6533
Grace Sutherland <rgs1690@gmail.com>
2021-08-31 17:20:32 -0500

    got authorview to work

--- src/javascripts/components/domBuilder.js
+++ src/javascripts/components/domBuilder.js
@@ -4,7 +4,7 @@ const domBuilder = () => {
     <div id="add-button"></div>
     <div id="form-container"></div>
     <div id="store"></div>
-    <div id="view"></div>
+    <div id="view">
   </div>`;
 };
 
